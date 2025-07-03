4be53c42f356ecfc472cf26bf00ab9d7ab0f0c5d
Laura Stampf <github@isadora.de>
2025-07-03 16:18:35 +0200

    Add helper functions

--- code/libCacheSim/cache/eviction/myHelpers.h
+++ code/libCacheSim/cache/eviction/myHelpers.h
@@ -16,4 +16,45 @@ static inline bool myCache_get(cache_t *cache, const request_t *req) {
     return cache_get_base(cache, req);
 }
 
+static inline cache_obj_t *myCache_insert_head(cache_t *cache, const request_t *req, cache_obj_t **head, cache_obj_t **tail) {
+    cache_obj_t *cache_obj = cache_insert_base(cache, req);     // insert obj (assuming it doesnt exist yet)
+    prepend_obj_to_head(head, tail, cache_obj);               // add the new obj to the head of the queue
+    return cache_obj;
+}
+
+static inline void myCache_evict_tail(cache_t *cache, cache_obj_t **head, cache_obj_t **tail) {
+    assert(*tail);
+
+    cache_obj_t *evict_obj = *tail; // evict oldest object at the tail
+    *tail = evict_obj->queue.prev;
+    
+    // edge case when queue is empty after removing single item in queue
+    if (!*tail) {
+        assert(cache->n_obj == 1);
+        *head = NULL; // make sure head does not
+    }
+    else {
+        assert(*tail);
+        (*tail)->queue.next = NULL;
+    }
+
+    assert(*head || !*head);
+
+    // finally, evict base of cache (tail item)
+    cache_evict_base(cache, evict_obj, true);
+}
+
+static inline bool myCache_remove_obj(cache_t *cache, const obj_id_t obj_id, cache_obj_t **head, cache_obj_t **tail) {
+    cache_obj_t *cache_obj = hashtable_find_obj_id(cache->hashtable, obj_id);
+
+    if (!cache_obj) {
+        return false;
+    }
+
+    remove_obj_from_list(head, tail, cache_obj);
+    cache_remove_obj_base(cache, cache_obj, true);
+
+    return true;
+}
+
 #endif // MYHELPERS_H
