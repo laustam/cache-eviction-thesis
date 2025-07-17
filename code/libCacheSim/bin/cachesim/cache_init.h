9824a04b8980c62a01cf503e16fd5763c43d6dba
Laura Stampf <github@isadora.de>
2025-07-17 15:05:53 +0200

    Remove unused C algs

--- code/libCacheSim/bin/cachesim/cache_init.h
+++ code/libCacheSim/bin/cachesim/cache_init.h
@@ -37,50 +37,8 @@ static inline cache_t *create_cache(const char *trace_path, const char *eviction
       {"my_lru",  myLRU_init},
       {"my_fifo", myFIFO_init},
       {"lru", LRU_init},
-      {"fifo", FIFO_init},
-      {"arc", ARC_init},
-      {"arcv0", ARCv0_init},
-      {"lhd", LHD_init},
-      {"random", Random_init},
-      {"randomTwo", RandomTwo_init},
-      {"lfu", LFU_init},
-      {"gdsf", GDSF_init},
-      {"lfuda", LFUDA_init},
-      {"twoq", TwoQ_init},
-      {"2q", TwoQ_init},
-      {"slru", SLRU_init},
-      {"slruv0", SLRUv0_init},
-      {"lecar", LeCaR_init},
-      {"lecarv0", LeCaRv0_init},
-      {"RandomLRU", RandomLRU_init},
-      {"cacheus", Cacheus_init},
-      {"size", Size_init},
-      {"lfucpp", LFUCpp_init},
-      {"wtinyLFU", WTinyLFU_init},
-      {"nop", nop_init},
-      {"fifo-reinsertion", Clock_init},
-      {"clock", Clock_init},
-      {"second-chance", Clock_init},
-      {"clockpro", ClockPro_init},
-      {"lirs", LIRS_init},
-      {"fifomerge", FIFO_Merge_init},
-      {"fifo-merge", FIFO_Merge_init},
-      {"flashProb", flashProb_init},
-      {"sfifo", SFIFO_init},
-      {"sfifov0", SFIFOv0_init},
-      {"lru-prob", LRU_Prob_init},
-      {"fifo-belady", FIFO_Belady_init},
-      {"lru-belady", LRU_Belady_init},
-      {"sieve-belady", Sieve_Belady_init},
-      {"s3lru", S3LRU_init},
-      {"s3fifo", S3FIFO_init},
-      {"s3-fifo", S3FIFO_init},
-      {"s3fifov0", S3FIFOv0_init},
-      {"s3-fifov0", S3FIFOv0_init},
-      {"s3fifod", S3FIFOd_init},
-      {"qdlp", QDLP_init},
-      {"CAR", CAR_init},
       {"sieve", Sieve_init},
+      {"fifo", FIFO_init}
   };
 
   cache_t *(*init_func)(common_cache_params_t, const char *) = NULL;
@@ -91,72 +49,8 @@ static inline cache_t *create_cache(const char *trace_path, const char *eviction
     }
   }
 
-  // Initializing for algorithms which require special handling (not in simple_algos)
   if (init_func) {
     cache = init_func(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "hyperbolic") == 0) {
-    cc_params.hashpower = MAX(cc_params.hashpower - 8, 16);
-    cache = Hyperbolic_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "tinyLFU") == 0) {
-    if (eviction_params == NULL) {
-      cache = WTinyLFU_init(cc_params, eviction_params);
-    } else {
-      const char *window_size = strstr(eviction_params, "window-size=");
-      if (window_size == NULL) {
-        char *new_params = malloc(strlen(eviction_params) + 20);
-        sprintf(new_params, "%s,window-size=0.01", eviction_params);
-        cache = WTinyLFU_init(cc_params, new_params);
-      } else {
-        cache = WTinyLFU_init(cc_params, eviction_params);
-      }
-    }
-  } else if (strcasecmp(eviction_algo, "belady") == 0 && strcasestr(trace_path, "lcs") == NULL) {
-    if (strcasestr(trace_path, "oracleGeneral") == NULL) {
-      WARN("belady is only supported for oracleGeneral and lcs trace\n");
-      WARN("to convert a trace to lcs format\n");
-      WARN("./bin/traceConv input_trace trace_format output_trace\n");
-      WARN("./bin/traceConv ../data/cloudPhysicsIO.txt txt\n");
-      exit(1);
-    }
-    cache = Belady_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "beladySize") == 0) {
-    if (strcasestr(trace_path, "oracleGeneral") == NULL && strcasestr(trace_path, "lcs") == NULL) {
-      WARN("beladySize is only supported for oracleGeneral and lcs trace\n");
-      WARN("to convert a trace to lcs format\n");
-      WARN("./bin/traceConv input_trace trace_format output_trace\n");
-      WARN("./bin/traceConv ../data/cloudPhysicsIO.txt txt\n");
-      exit(1);
-    }
-    cc_params.hashpower = MAX(cc_params.hashpower - 8, 16);
-    cache = BeladySize_init(cc_params, eviction_params);
-#ifdef ENABLE_3L_CACHE
-  } else if (strcasecmp(eviction_algo, "3LCache") == 0) {
-    cache = ThreeLCache_init(cc_params, eviction_params);
-#endif
-#ifdef ENABLE_GLCACHE
-  } else if (strcasecmp(eviction_algo, "GLCache") == 0 || strcasecmp(eviction_algo, "gl-cache") == 0) {
-    cache = GLCache_init(cc_params, eviction_params);
-#endif
-#ifdef ENABLE_LRB
-  } else if (strcasecmp(eviction_algo, "lrb") == 0) {
-    cache = LRB_init(cc_params, eviction_params);
-#endif
-#ifdef INCLUDE_PRIV
-  } else if (strcasecmp(eviction_algo, "mclock") == 0) {
-    cache = MClock_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "lp-sfifo") == 0) {
-    cache = LP_SFIFO_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "lp-arc") == 0) {
-    cache = LP_ARC_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "lp-twoq") == 0) {
-    cache = LP_TwoQ_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "qdlpv0") == 0) {
-    cache = QDLPv0_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "s3fifodv2") == 0) {
-    cache = S3FIFOdv2_init(cc_params, eviction_params);
-  } else if (strcasecmp(eviction_algo, "myMQv1") == 0) {
-    cache = myMQv1_init(cc_params, eviction_params);
-#endif
   } else {
     ERROR("do not support algorithm %s\n", eviction_algo);
     abort();
