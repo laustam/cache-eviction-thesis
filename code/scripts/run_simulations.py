a46e7f81afe120c9cc1644a6026977f4b62d74aa
Laura Stampf <github@isadora.de>
2025-07-21 14:50:12 +0200

    Specify output path for results in run_simulations.py script

--- code/scripts/run_simulations.py
+++ code/scripts/run_simulations.py
@@ -8,7 +8,11 @@ def main() -> None:
     for filename in os.listdir(data_dir):
         file_path = os.path.join(data_dir, filename)
         if os.path.isfile(file_path):
-            cmd = [bin_path, file_path, 'txt', 'my_sieve,my_lru,my_fifo', 'auto']
+            # Ensure results dir actually exists since cachesim does not create a new directory!
+            results_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
+            os.makedirs(results_dir, exist_ok=True)
+
+            cmd = [bin_path, file_path, 'txt', 'my_sieve,my_lru,my_fifo', 'auto', f'--output=../results/{filename[:-4]}_results.txt']
             subprocess.run(cmd)
 if __name__ == "__main__":
     main()
