b1d34b5db6880c9ecc9d0a8a316827672f590698
Laura Stampf <github@isadora.de>
2025-08-26 23:51:39 +0200

    Updated run simulations script with correct rel cache sizes

--- code/scripts/run_simulations.py
+++ code/scripts/run_simulations.py
@@ -3,7 +3,7 @@ import subprocess
 import time
 from dir_paths import DATA_DIR, CACHESIM_PATH, RESULTS_DIR
 
-REL_CACHE_SIZES = [0.0001, 0.0002, 0.0004, 0.0008, 0.001, 0.002, 0.003, 0.004, 0.008, 0.01, 0.02, 0.03, 0.04, 0.08, 0.1, 0.2, 0.3, 0.4, 0.8]
+REL_CACHE_SIZES = [0.0001, 0.0002, 0.0004, 0.0006, 0.0008, 0.001, 0.002, 0.004, 0.006, 0.008, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1, 0.2, 0.4, 0.6, 0.8]
 
 def main() -> None:
     # Ensure results dir actually exists since cachesim does not create a new directory!
@@ -22,7 +22,7 @@ def main() -> None:
 
         if os.path.isfile(file_path):
             base_filename = os.path.splitext(filename)[0]
-            cmd = [CACHESIM_PATH, file_path, 'txt', 'my_fifo,my_lru,my_sieve', ','.join(str(x) for x in REL_CACHE_SIZES), f'--output={RESULTS_DIR}/{base_filename}_results.txt']
+            cmd = [CACHESIM_PATH, file_path, 'txt', 'myFIFO,myLRU,mySIEVE', ','.join(str(x) for x in REL_CACHE_SIZES), f'--output={RESULTS_DIR}/{base_filename}_results.txt']
             subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 if __name__ == "__main__":
